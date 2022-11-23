6d4a690f5a600bc9904b15b35291ddc06cfe87d2
marcelli-lima <marcelli_lima@hotmail.com>
2022-11-23 13:02:44 -0300

    header usuario logado

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,31 +3,20 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../api";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
-  const username = useForm("email");
+  const username = useForm();
   const password = useForm();
-  console.log(password.value);
 
-  function handleSubmit(event) {
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-          return json;
-        });
+      userLogin(username.value, password.value);
     }
   }
 
